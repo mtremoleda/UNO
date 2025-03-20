38be911c6bf7734685764021f105126ab932fa62
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-03-20 09:54:09 +0100

    commit carta i jugador 1r

--- UNO/Carta.java
+++ UNO/Carta.java
@@ -1,4 +1,16 @@
 package UNO;
 
 public class Carta {
+    public int numero;
+    public enum color{
+        Groc, Vermell, Blau, Verd
+    }
+
+    public Carta(int numero){
+        this.numero = numero;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
 }
--- UNO/Jugador.java
+++ UNO/Jugador.java
@@ -1,4 +1,20 @@
 package UNO;
 
 public class Jugador {
+    String nom;
+    public Carta[] cartes;
+
+    public Jugador (String nom, Carta[] cartes){
+        this.nom = nom;
+        this.cartes = cartes;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public Carta[] getCartes(){
+        return cartes;
+    }
+
 }
