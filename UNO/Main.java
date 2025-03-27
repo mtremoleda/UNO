7237ad915c695a1d57af3ed5f977206d3022c829
marcel tremoleda <mtremoleda192@boscdelacoma.cat>
2025-03-27 12:02:58 +0100

    Main i UI

--- UNO/Main.java
+++ UNO/Main.java
@@ -1,4 +1,27 @@
 package UNO;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Mazo mazo = new Mazo();
+        mazo.barrejar();
+
+
+        Pilo pilo = new Pilo();
+
+        Jugador j1 = new Jugador("Jugador 1");
+
+
+        System.out.println("Cartas del Mazo:");
+
+        for (int i = 0; i < mazo.getCartes().size(); i++) {
+            UI.mostrarCarta(mazo.getCartes().get(i));
+        }
+
+        System.out.println("Cartas del Jugador1");
+        for (Carta c : mazo.getCartes()){
+            j1.addCarta(c);
+        }
+        UI.mostrarCartes(j1.getCartes());
+    }
 }
